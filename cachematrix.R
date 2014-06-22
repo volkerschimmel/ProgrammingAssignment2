34d888fd5bf9d0414e28fcb804461faa7a0f10be
volkerschimmel <v.schimmel@gmail.com>
2014-06-22 22:00:53 +0300

    Edited file - Programming Assignment no.2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The function creates two matrices which will be combined into a list in the end
+## At first it creates the original to be stored in 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  org<-function(){
+    x<<-y
+  }
+  inv<-function(){
+    solve(y)
+  }
+  matrices<<-list(org = org, inv = inv)
 }
 
 
-## Write a short comment describing this function
+## The $inverse part of the returned list is tested and if not zero returned
+## If $inverse is empty, it means that the inverse has to be calculated first, which is the remainder of the function 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-matrices$inv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  x$inv(x)
+  matrices$inv(x)
 }
